a39d392069cacc653bd95be2fdf6a57fb61dabb0
Marcus <codiestm@gmail.com>
2024-04-23 20:02:40 +0200

    login page

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.render("index", { username });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} - http://localhost:${PORT}`);
 });
