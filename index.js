14cc10bc2b261d68d482f4dd0548d00fe2897df3
Marcus <codiestm@gmail.com>
2024-04-28 20:59:23 +0200

    fixat knapparna åt er!

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-const routes = require('./routes');
-app.use('/', routes);
+const routes = require("./routes");
+app.use("/", routes);
 
 const PORT = process.env.PORT || 5000;
 
 // Huvudsidan för att logga in
 app.get("/", (req, res) => {
   res.render("login");
+});
+
+app.get("/view", (req, res) => {
+  res.render("index");
 });
 
 // Vart användaren kommer efter att ha loggat in, vilket är flödesidan såklart!
