9d0cfaa55f9e33ecd919a6e17335fec1911b5c86
Marcus <codiestm@gmail.com>
2024-04-28 21:21:09 +0200

    comment & like functions to be made - and moved the data to a seperate file called "data.json" for easier usage until the database is fully operational.

---
+++ public/js/interactionHandler.js
@@ -0,0 +1,22 @@
+/*
+  @description: Fetches the post with the given id and adds the comment to the post
+  @param: id - the id of the post
+  @param: user - the user who is commenting
+  @param: comment - the comment to be added to the post
+  @return: the updated post
+  @run: location.reload();
+*/
+const comment = (id, user, comment) => {
+    return;
+};
+
+/*
+    @description: Fetches the post with the given id and adds the like to the post
+    @param: id - the id of the post
+    @param: user - the user who is liking the post
+    @return: the updated post
+    @run: location.reload();
+*/
+const like = (id, user) => {
+    return;
+};
