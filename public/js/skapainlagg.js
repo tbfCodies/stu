92cc3a56fbc82c040bfddd011902c59ccf367652
JuliaBergstrom <ujiebp-1@student.ltu.se>
2024-04-29 11:24:00 +0200

    Nu visas även bilden man laddat upp

---
+++ public/js/skapainlagg.js
@@ -0,0 +1,18 @@
+const chooseFile = document.getElementById("bild");
+const imgPreview = document.getElementById("bild-forhandsvisning");
+
+chooseFile.addEventListener("change", function () {
+    getImgData();
+});
+
+function getImgData() {
+    const files = chooseFile.files[0];
+    if (files) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(files);
+        fileReader.addEventListener("load", function () {
+            imgPreview.style.display = "block";
+            imgPreview.innerHTML = '<img src="' + this.result + '" />';
+        });
+    }
+}
