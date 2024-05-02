f57a64e56e330037d43175502b03b07d561e0cbb
ElmerTg <98717990+ElmerTallgren@users.noreply.github.com>
2024-05-02 11:54:27 +0200

    Filter knapparna kan typ användas. Men behöver fortfarande koppla dem till inläggen som visas på sidan.

--- public/js/script.js
+++ public/js/script.js
@@ -4,17 +4,60 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function () {
       buttons.forEach((btn) => btn.classList.remove("active"));
       this.classList.add("active");
-      console.log("klickade på: " + this.textContent);
+      console.log("Clicked on: " + this.textContent);
     });
   });
 
-  // Väljer campus första gången man öpnnar sidan
   const campusButton = document.querySelector(".campusBTN");
-  if (campusButton) {
+  const vannerButton = document.querySelector(".vannerBTN");
+
+  if (campusButton && vannerButton) {
+    campusButton.addEventListener("click", handleCampusButtonClick);
+    vannerButton.addEventListener("click", handleVannerButtonClick);
+
+    // Välj CAMPUS första gången.
     campusButton.classList.add("active");
     console.log("Default button set to active: " + campusButton.textContent);
   }
 });
+
+function handleCampusButtonClick() {
+  const vannerButton = document.querySelector(".vannerBTN");
+  if (vannerButton.classList.contains("active")) {
+    vannerButton.classList.remove("active");
+    console.log("Deselecting Vänner button");
+  }
+  console.log("Campus button clicked");
+  loadCampusPosts();
+}
+
+function handleVannerButtonClick() {
+  const campusButton = document.querySelector(".campusBTN");
+  if (campusButton.classList.contains("active")) {
+    campusButton.classList.remove("active");
+    console.log("Deselecting Campus button");
+  }
+  console.log("Vänner button clicked");
+  loadFriendPosts();
+}
+
+function loadCampusPosts() {
+  // Hämta campus inlägg
+  console.log("Hämta campus inlägg....");
+  updatePosts("{id: 151581}");
+}
+
+function loadFriendPosts() {
+  // Hämta vänner inlägg
+  console.log("Hämta vänner inlägg....");
+  updatePosts("{id: 131231}");
+}
+
+function updatePosts(posts) {
+  // Updatera inläggen
+  console.log("Uppdaterar inläggen");
+  console.log(posts);
+}
 
 //Funktion för att öppna menyn till höger
 function openSuggestionsMeny() {
