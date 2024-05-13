56ebfd628887f44e4396b8b1eec066c1f519094d
JuliaBergstrom <ujiebp-1@student.ltu.se>
2024-05-13 11:46:32 +0200

    Lite kommentarer

--- public/js/skapainlagg.js
+++ public/js/skapainlagg.js
@@ -1,16 +1,21 @@
+//Hämta referenser
 const chooseFile = document.getElementById("bild");
 const imgPreview = document.getElementById("bild-forhandsvisning");
 
+//Om en bild laddats upp anropas funktionen getImgData
 chooseFile.addEventListener("change", function () {
     getImgData();
 });
 
+//Hämta bilddata
 function getImgData() {
     const files = chooseFile.files[0];
+    //Kontrollera om en bild laddats upp
     if (files) {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(files);
         fileReader.addEventListener("load", function () {
+            //Visa bilden som laddats upp
             imgPreview.style.display = "block";
             imgPreview.innerHTML = '<img src="' + this.result + '" />';
         });
